c92b5431966b02fc2149ab1c14f03260f00427a5
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-13 16:41:01 +0100

    css changes

--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,7 @@
-// const title = document.querySelector('#note-title');
+const title = document.querySelector('#note-title');
 const content = document.querySelector('#note-content');
 const submitButton = document.querySelector('#submit');
 const newNote = document.querySelector('#new-note');
-
-const title = document.querySelector('#note-title');
-title.style = "background: green;";
 
 title.addEventListener('click', () => {
   content.style = "display: inline-block;";

66a8b91f521e612bfc7bfa6036f0d486c0653c32
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-14 16:58:00 +0100

    create input field

--- src/interface.js
+++ src/interface.js
@@ -1,53 +1,36 @@
-const title = document.querySelectorAll(".note-title");
 const input = document.querySelector("#note-input");
 const submitButton = document.querySelector("#submit");
-const newNote = document.querySelector("#new-note");
+const title = document.querySelectorAll(".note-title");
+const noteContent = document.querySelectorAll(".note-content");
 // const notes;
 document.addEventListener("DOMContentLoaded", () => {
   const notes = new Notes();
 
   // title.addEventListener("click", () => {
   //   // logic for when content is visible and logic for when it's hidden needed
-  //   content.style = "display: inline-block;";
-  //   submitButton.style = "display: inline-block;";
+  //   noteContent.style = "display: inline-block;";
   // });
 
   // After clicking newNote a new div element will be added to the DOM
   // It will have a title, textarea and submit button
 
   submitButton.addEventListener("click", () => {
+    if (input.value === "") {
+      return;
+    }
     notes.create(input.value);
     const notesList = document.querySelector(".notes-list");
+
     let newTitle = document.createElement("h4");
     newTitle.classList.add("note-title");
     newTitle.innerText = `${notes.title(input.value)}...`;
     notesList.appendChild(newTitle);
 
-    // <!-- <h4 id="note-title" class="note-title">Note title</h4> -->
+    let noteBody = document.createElement("input");
+    noteBody.classList.add("note-content");
+    noteBody.value = input.value;
+    notesList.appendChild(noteBody);
 
-    // container = document.querySelector(".container");
-    // let newContainer = document.createElement("div");
-    // newContainer.setAttribute("class", "note-container");
-    // let newTitle = document.createElement("h3");
-    // newTitle.innerHTML = "hello";
-    // newTitle.setAttribute("id", "note-title");
-    // newTitle.setAttribute("class", "note-title");
-
-    // let newTextArea = document.createElement("textarea");
-    // newTextArea.setAttribute("id", "note-input");
-    // newTextArea.setAttribute("class", "note-input");
-    // newTextArea.setAttribute("cols", "30");
-    // newTextArea.setAttribute("rows", "10");
-    // newTextArea.setAttribute("placeholder", "Write A Note");
-
-    // let newButton = document.createElement("button");
-    // newButton.setAttribute("id", "submit");
-    // newButton.setAttribute("class", "submit button");
-    // newButton.innerHTML = "Submit";
-    // container.appendChild(newContainer);
-    // newContainer.appendChild(newTitle);
-    // newContainer.appendChild(newTextArea);
-    // newContainer.appendChild(document.createElement("br"));
-    // newContainer.appendChild(newButton);
+    input.value = "";
   });
 });

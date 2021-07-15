3e430f65aaf6b4d12b47f5ba388f67daee374140
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-15 15:54:03 +0100

    shows/hides note content on title click

--- src/interface.js
+++ src/interface.js
@@ -1,31 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const notes = new Notes();
   const input = document.querySelector("#note-input");
   const submitButton = document.querySelector("#submit");
   const titles = document.querySelectorAll(".note-title");
   const noteContent = document.querySelectorAll(".note-content");
-  const notes = new Notes();
 
-  console.log(titles);
-  console.log(noteContent);
-
-  titles.forEach(function(title) {
-    console.log(title);
-    // console.log(titles.indexOf(title));
-    // trying to hide the input when clicking the title
-    let i = 0;
+  // Shows/hides the note content when clicking title
+  titles.forEach(function(title, index) {
     title.addEventListener("click", () => {
-      console.log(title);
-      // logic for when content is visible and logic for when it's hidden needed
-      title.style = "background: red;";
-      // noteContent.style = "background: red;";
-      noteContent[i].style.display = "inline-block";
+      noteContent[index].classList.toggle("hide");
     });
-    i++;
   });
 
   // After clicking newNote a new div element will be added to the DOM
   // It will have a title, textarea and submit button
-
   submitButton.addEventListener("click", () => {
     if (input.value === "") {
       return;
@@ -44,9 +32,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let noteBody = document.createElement("input");
     noteBody.classList.add("note-content");
+    noteBody.classList.add("hide");
     noteBody.value = input.value;
     notesList.appendChild(noteBody);
 
     input.value = "";
+
+    newTitle.addEventListener("click", () => {
+      noteBody.classList.toggle("hide");
+    });
   });
 });

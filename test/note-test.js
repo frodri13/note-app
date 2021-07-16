419d77cca1baf96f5f80fc76ca82d6dd6d1701ba
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-07-16 14:20:12 +0100

    Rearranged original test comments

--- test/note-test.js
+++ test/note-test.js
@@ -1,28 +1,3 @@
-// function verify(suiteName, fn) {
-//   try {
-//     console.log(`suite: ${suiteName}`);
-//     fn();
-//   } catch (err) {
-//     console.log(err.mesage);
-//   }
-// };
-
-// verify("Notes", function () {
-//   it("slices the content of the note by 20 characters", function () {
-//     let notes = new Notes();
-//     notes.create(
-//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis ratione totam dignissimos molestiae incidunt eum illum ipsum distinctio provident possimus!"
-//     );
-//     checkIf(notes.title(notes.list[0].title).length).is(20);
-//   });
-
-//     it("can store a list of notes", function () {
-//       let notes = new Notes();
-//       notes.create("hello");
-
-//       checkIf(notes.list[0].content).is("bye");
-//     });
-// });
 const note = new Notes();
 
 const firstTest = () => {
@@ -58,3 +33,28 @@ first.addEventListener("click", firstTest)
 second.addEventListener("click", secondTest)
 third.addEventListener("click", thirdTest)
 
+// function verify(suiteName, fn) {
+//   try {
+//     console.log(`suite: ${suiteName}`);
+//     fn();
+//   } catch (err) {
+//     console.log(err.mesage);
+//   }
+// };
+
+// verify("Notes", function () {
+//   it("slices the content of the note by 20 characters", function () {
+//     let notes = new Notes();
+//     notes.create(
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis ratione totam dignissimos molestiae incidunt eum illum ipsum distinctio provident possimus!"
+//     );
+//     checkIf(notes.title(notes.list[0].title).length).is(20);
+//   });
+
+//     it("can store a list of notes", function () {
+//       let notes = new Notes();
+//       notes.create("hello");
+
+//       checkIf(notes.list[0].content).is("bye");
+//     });
+// });
